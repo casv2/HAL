3129abffc3939fb55e04dc858ddcd88cdc1ac6aa
C.A.S. Van Der Oord <casv2@womble0.eng.cam.ac.uk>
2021-02-11 21:35:52 +0000

    new CASTEP

--- src/CALC.jl
+++ src/CALC.jl
@@ -25,20 +25,21 @@ function CASTEP_calculator(at)
     py_at = ASEAtoms(at)
 
     calculator = CASTEP()
-    calculator[:_castep_command] = "mpirun -n 8 /opt/castep/19.11/castep.mpi"
+    calculator[:_castep_command] = "mpirun -n 8 /home/vc381/castep/castep.mpi"
     calculator[:_directory] = "./_CASTEP"
-    calculator.param[:cut_off_energy] = 500
+    calculator.param[:cut_off_energy] = 400
     calculator.param[:calculate_stress] = true
     calculator.param[:smearing_width] = 0.1
     calculator.param[:finite_basis_corr] = "automatic"
     calculator.param[:mixing_scheme] = "Pulay"
     calculator.param[:write_checkpoint] = "none"
-    calculator.cell[:kpoints_mp_spacing] = 0.1
+    #calculator.cell[:kpoints_mp_spacing] = 0.1
+    calculator.cell[:kpoint_mp_grid] = "1 1 1"
     py_at.po[:set_calculator](calculator)
 
     E = py_at.po.get_potential_energy()
     F = py_at.po.get_forces()
-    V = -1.0 * py_at.po.get_stress() * py_at.po.get_volume()
+    V = -1.0 * py_at.po.get_stress(voigt=false) * py_at.po.get_volume()
 
     write_xyz("./temp.xyz", py_at)
 
