48428cfd314d33524fcc4858eba621524d567f96
casv2 <casv2@cam.ac.uk>
2021-03-23 13:45:40 +0100

    table option of LML

--- src/OPTIM.jl
+++ src/OPTIM.jl
@@ -37,6 +37,31 @@ function get_lml(N, deg, Binfo, Vref, weights, al)
     return HMD.BRR.log_marginal_likelihood(Ψ, Y, α, β)
 end
 
+function find_N_deg_table(Binfo, Vref, weights, al; Ns=[2,3,4,5], degs=[8,9,10,11])
+    _lml = zeros(length(degs), length(Ns))
+    _D = Dict()
+
+    for (i,N) in enumerate(Ns)
+        for (j,deg) = enumerate(degs)
+            _lml[j,i] = round(get_lml(N, deg, Binfo, Vref, weights, al), digits=2)
+            _D[(N, deg)] = _lml[j,i]
+        end
+    end
+
+    _, (maxN, maxdeg) = findmax(_D)
+
+    hl = Highlighter((lml,i,j)->true,
+                            (h,lml,i,j)->begin
+                                color = get(colorschemes[:coolwarm], lml[i,j], (minimum(_lml),maximum(_lml)))
+                                return Crayon(foreground = (round(Int,color.r*255),
+                                                            round(Int,color.g*255),
+                                                            round(Int,color.b*255)))
+                            end)
+
+    pretty_table(_lml, Ns, row_names = degs, highlighters = hl)
+
+    return _lml
+end
 
 function find_N_deg(Binfo, Vref, weights, al; Ns=[2,3,4,5], degs=[8,9,10,11])
     _lml = zeros(length(degs), length(Ns))
