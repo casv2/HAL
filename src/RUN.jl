cc3de5c17f12c5e6dd96cc4e244b646b119fff2c
casv2 <casv2@cam.ac.uk>
2021-06-16 16:37:45 +0100

    refit option

--- src/RUN.jl
+++ src/RUN.jl
@@ -70,8 +70,12 @@ function run_HMD(B, Vref, weights, al, start_configs, run_info, calc_settings)#,
 
             # B = JuLIP.MLIPs.IPSuperBasis([Bpair, Bsite]);
 
-            if m % run_info["refit"] == 1
-                global IP, k = do_fit(B, Vref, al, weights, run_info["ncoms"])
+            if haskey(run_info, "refit")
+                if m % run_info["refit"] == 1
+                    global IP, k = do_fit(B, Vref, al, weights, run_info["ncoms"])
+                end
+            else
+                IP, k = do_fit(B, Vref, al, weights, run_info["ncoms"])
             end
 
             if config_type ∉ keys(run_info)
