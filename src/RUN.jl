e734793b62e656821a661a87b5727d552a0b8853
casv2 <casv2@cam.ac.uk>
2021-07-20 12:19:29 +0100

    adding shear steps

--- src/RUN.jl
+++ src/RUN.jl
@@ -166,7 +166,7 @@ function vol_step(at)
     d = Normal()
 	scale = 1 + (rand(d) * 0.01)
     C1 = at.cell
-    C2 = at.cell*scale .+ rand(d, (3,3)) * 0.01
+    C2 = at.cell*scale .* rand(d, (3,3)) * 0.01
     s = C2 / C1
     for i in 1:length(at)
         at.X[i] = at.X[i]' * s
