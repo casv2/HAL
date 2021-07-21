6738dba8ed681f580f378a6b68b25d27f78c10e9
casv2 <casv2@cam.ac.uk>
2021-07-21 14:27:36 +0100

    heat true

--- src/RUN.jl
+++ src/RUN.jl
@@ -108,7 +108,8 @@ function run_HMD(Vref, weights, al, start_configs, run_info, calc_settings, Binf
                     maxp=run_info[config_type]["maxp"],
                     γ=run_info[config_type]["γ"],
                     swap=run_info[config_type]["swap"],
-                    vol=run_info[config_type]["vol"])
+                    vol=run_info[config_type]["vol"],
+                    vol=run_info[config_type]["heat"])
                     #minR=run_info[config_type]["minR"])
             
             plot_HMD(E_tot, E_pot, E_kin, T, P, m, k=1)
@@ -215,7 +216,7 @@ function get_site_uncertainty(IP, IPs, at)
     return p, energy(IP, at)
 end
 
-function run(IP, Vref, B, k, at; γ=0.02, nsteps=100, temp=0, dt=1.0, τstep=50, dτ=0.01, maxp=0.15, minR=2.0, var=true, A=1e-2, swap=false, vol=false)
+function run(IP, Vref, B, k, at; γ=0.02, nsteps=100, temp=0, dt=1.0, τstep=50, dτ=0.01, maxp=0.15, minR=2.0, var=true, A=1e-2, heat=false, swap=false, vol=false)
     E_tot = zeros(nsteps)
     E_pot = zeros(nsteps)
     E_kin = zeros(nsteps)
