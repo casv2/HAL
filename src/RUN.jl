34bf44e6bfdbd75ff6045bbb910da9716734079f
casv2 <casv2@cam.ac.uk>
2021-11-02 10:57:15 +0000

    0.005

--- src/RUN.jl
+++ src/RUN.jl
@@ -183,9 +183,9 @@ end
 
 function vol_step(at)
     d = Normal()
-	scale = 1 + (rand(d) * 0.001)
+	scale = 1 + (rand(d) * 0.005)
     C1 = at.cell
-    C2 = at.cell*scale .+ (rand(d, (3,3)) * 0.001)
+    C2 = at.cell*scale .+ (rand(d, (3,3)) * 0.005)
     s = C2 / C1
     for i in 1:length(at)
         at.X[i] = at.X[i]' * s
