85738a3a63545b5649de7e042c09d3a2dc274afa
casv2 <casv2@cam.ac.uk>
2022-05-27 16:03:29 +0100

    fit_intercept=false

--- src/BRR.jl
+++ src/BRR.jl
@@ -36,7 +36,7 @@ end
 function maxim_hyper(Ψ, Y, alpha_init, beta_init; brrtol=1e-3)
     BRR = pyimport("sklearn.linear_model")["BayesianRidge"]
 
-    clf = BRR(tol=brrtol, alpha_init=alpha_init, lambda_init=beta_init, compute_score=true)
+    clf = BRR(tol=brrtol, alpha_init=alpha_init, lambda_init=beta_init, fit_intercept=false, compute_score=true)
     clf.fit(Ψ, Y)
 
     α = clf.alpha_
