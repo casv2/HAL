fcc5fd97043d4ca94384241424787bdabf09cf08
casv2 <casv2@cam.ac.uk>
2022-07-16 14:14:20 +0100

    HAL commit

--- src/RUN.jl
+++ src/RUN.jl
@@ -273,8 +273,9 @@ function run(IP, Vref, B, k, at; γ=0.02, nsteps=100, temp=300, dt=1.0, rτ=0.5,
         mFs[i] = mean(norm.(F))
 
         if i > 100
-            @show τ, (rτ * mean(mFs[i-99:i])), tanh( (rτ * mean(mFs[i-99:i])) / 10.0 )
-            τ = (rτ * mean(mFs[i-99:i])) / mean(mvarFs[i-99:i])
+            @show τ, mean(mFs[i-99:i]), tanh( mean(mFs[i-99:i]) / 5.0 )
+            #τ = (rτ * mean(mFs[i-99:i])) / mean(mvarFs[i-99:i])
+            τ = (rτ * tanh( mean(mFs[i-99:i]) / 5.0 )) / mean(mvarFs[i-99:i])
         else
             τ = 0.0
         end
